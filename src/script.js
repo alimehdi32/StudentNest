5e3ab6027ec528d8bba06cf63710e43b1b22b021
Ali Mehdi <alimehdi432faiz@gmail.com>
2025-02-01 17:37:05 +0530

    ..

---
+++ src/script.js
@@ -0,0 +1,12 @@
+let property = document.querySelector(".group")
+console.log(property)
+property.addEventListener("click", (e) => {
+    let dropdown = document.querySelector(".dropdown")
+    if (dropdown.classList.contains("hidden")) {
+        dropdown.classList.remove("hidden");
+        dropdown.classList.add("block");
+    } else {
+        dropdown.classList.remove("block");
+        dropdown.classList.add("hidden");
+    }
+})
